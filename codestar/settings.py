3008c2b45a09ad6e1f334c4d00d0c1c09da7fc69
KarlPreisler <Karlpreisler@gmail.com>
2023-01-10 14:08:40 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['karl-django-blog.herokuapp.com', 'localhost']
 ACCOUNT_EMAIL_VERIFICATION = 'none'
